d83b92abf717fa2a55f006653584e6cd81394a96
blogdopata <vcaballero_c@hotmail.com>
2018-08-22 04:17:19 -0500

    views Login

---
+++ js/myscripts.js
@@ -0,0 +1,14 @@
+var btnContainer = document.getElementById("menu");
+
+
+var btns = btnContainer.getElementsByClassName("menu-link");
+console.log(btns);
+
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
