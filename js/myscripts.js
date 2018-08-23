3614608078fa70ce06d5b3953641d714559d259f
blogdopata <vcaballero_c@hotmail.com>
2018-08-23 05:53:43 -0500

    cvView

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,3 +1,6 @@
+
+/* Links activos */
+
 var btnContainer = document.getElementById("menu");
 
 
@@ -12,3 +15,57 @@ for (var i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+/* End  Links activos */
+
+/*  upload y replace de image -  validar lado backend */
+
+var input = document.querySelector('input[type=file]');
+    
+input.onchange = function () {
+    var file = input.files[0];
+
+    drawOnCanvas(file);   
+    displayAsImage(file); 
+};
+
+function drawOnCanvas(file) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        var dataURL = e.target.result,
+            c = document.querySelector('canvas'), 
+            ctx = c.getContext('2d'),
+            img = new Image();
+
+        img.onload = function () {
+            c.width = img.width;
+            c.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        };
+
+        img.src = dataURL;
+    };
+
+   
+}
+
+function displayAsImage(file) {
+    var imgURL = URL.createObjectURL(file),
+        img = document.createElement('img'),
+        imgchange = document.getElementById("upfile1");
+        console.log(imgchange);
+
+    img.onload = function () {
+        URL.revokeObjectURL(imgURL);
+    };
+
+    imgchange.src = imgURL;
+    
+}
+
+$("#upfile1").click(function () {
+    $("#file1").trigger('click');
+});
+
+/*  End  upload y replace de image */
