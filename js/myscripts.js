156db8437abe0fa825a99c521d023cc67441b5a4
blogdopata <vcaballero_c@hotmail.com>
2018-08-27 22:08:22 -0500

    OtherViewrs

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,14 +4,14 @@
 var btnContainer = document.getElementById("menu");
 
 
-var btns = btnContainer.getElementsByClassName("menu-link");
+var btns = document.getElementsByClassName("menu-link");
 console.log(btns);
 
 
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
+        current[0].className = current[0].className.replace("active", "");
         this.className += " active";
     });
 }
